37a937d8092ac0cb5714dbf68df729be0232f175
Tai <5506726tai@gmail.com>
2023-12-15 15:47:52 +0700

    Feat Ex finish

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,41 @@
-class EmailNotification { }
+/*ex
+class Dog {
+  constructor(name, breed, sex, birthYear) {
+    this.name = name;
+    this.breed = breed;
+    this.sex = sex;
+    this.birthYear = birthYear;
+  }
+}*/
 
-class SMSNotification { }
+class EmailNotification { 
+    constructor(notificationId,createdTime,content,receiver){
+     this.notificationId = notificationId;
+     this.createdTime = createdTime;
+     this.content = content;
+     this.receiver = receiver;
+    }
+    send() {
+    console.log("Notification has been sent to"+this.receiver)
+    }
+}
+const emailRecei = new EmailNotification(555,12.31,"otw","Tai1");
+//console.log(emailRecei)
+emailRecei.send()
+
+
+class SMSNotification { 
+    constructor(notificationId,createdTime,content,phoneNumber ) {
+     this.notificationId = notificationId;
+     this.createdTime = createdTime;
+     this.content = content;
+     this.phoneNumber = phoneNumber;
+    }
+    send( ) {
+      console.log("Notification has been sent to"+this.phoneNumber)   
+    }
+}
+
+const smsRecei = new SMSNotification(444,15.35,"met",1150)
+//console.log(smsRecei)
+smsRecei.send()
--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,31 @@
-class Notification { }
+class Notification {
+    constructor(notificationId,createdTime,content,receiver ) {
+       this.notificationId = notificationId;
+       this.createdTime = createdTime;
+       this.content = content;
+       this.receiver = receiver;
+    }
+    send( ) {
+        console.log("Notification has been sent to"+this.receiver)   
+      }
+ }
 
-class EmailNotification { }
+class EmailNotification extends Notification {
+    constructor(notificationId,createdTime,content,receiver){
+      super(notificationId,createdTime,content,receiver)
+       }
+       
+ }
 
-class SMSNotification { }
+class SMSNotification extends Notification {
+    constructor(notificationId,createdTime,content,phoneNumber ) {
+       super(notificationId,createdTime,content,phoneNumber)
+       }
+      
+ }
+
+ const emailRecei = new EmailNotification(555,12.31,"otw","Tai1");
+ //console.log(emailRecei)
+ emailRecei.send()
+
+ 
